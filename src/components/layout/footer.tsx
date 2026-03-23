c4e3091ac7a4f98ef37721e505551d593f06285f
Yasmin Alves <yasmin.alves16@outlook.com.br>
2026-03-23 00:12:12 -0300

    feat: create base layout components

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,15 @@
+export function Footer() {
+  return (
+    <footer>
+      <div>
+        {/* <img src="/logo.png" alt="Logo" /> */}
+        <p>Feito com 💖 por Yasmin Alves</p>
+      </div>
+      <div>
+        <p>
+          <small>© 2026 Yasmin Alves.</small>
+        </p>
+      </div>
+    </footer>
+  );
+}
