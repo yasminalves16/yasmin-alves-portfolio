89bb5dfb7d04ebc9108ed26f2b811fadc7aeb7a8
Yasmin Alves <yasmin.alves16@outlook.com.br>
2026-06-30 20:27:41 -0300

    refactor: organize portfolio data into domain folders

---
+++ src/data/media-recognition/en-US.ts
@@ -0,0 +1,16 @@
+import type { MediaRecognition } from '../../types/media-recognition';
+
+export const mediaRecognition: MediaRecognition[] = [
+  {
+    id: 'qualifica-sp-mulheres-tech',
+    source: 'Government of the State of São Paulo',
+    sourceSubtitle: 'Secretariat of Economic Development',
+    title:
+      'One in four women enrolled in Qualifica SP seeks professional courses in Information Technology',
+    description:
+      'Featured in an official São Paulo Government article about women in technology, with an interview about my career path and the importance of professional qualification.',
+    tags: ['Women in Tech', 'Qualifica SP', 'Google Cloud'],
+    articleUrl:
+      'https://www.desenvolvimentoeconomico.sp.gov.br/DesenvolvimentoEconomico/noticias/uma+em+cada+quatro+mulheres+inscritas+no+qualifica+sp+busca+por+cursos+profissionalizantes+na+area+de+tecnologia+da+informacao'
+  }
+];
